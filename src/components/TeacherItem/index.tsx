54cb229122f0c8f347760815a42d72dd2d157ffb
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-08-06 21:39:03 -0400

    Add components Input, Select and Textarea Change TeacherItem to receive props Finish pages Landing, TeacherForm and TeacherList Add service to connect with API

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+  subject: string;
+  cost: number;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://pbs.twimg.com/profile_images/1275367613640708097/WBW4hV0m_400x400.jpg" alt="doge"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Doge</strong>
-          <span>Bark</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Cute doggo.
-        <br/> <br/>
-        Bark all the time and cute things.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 5,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          onClick={createNewConnection} 
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://wa.me/055${teacher.whatsapp}?text=Quero%20pegar%20uma%20aula%20monstra%20contigo%21`}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
