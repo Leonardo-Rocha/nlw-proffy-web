1b3191d2a7d9886f5a237743977886a2fd8bea14
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-08-08 22:20:17 -0400

    Change api to change depending on environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'development'
+? 'http://192.168.2.102:3333'
+: 'https://nlw-proffy-api-rest.herokuapp.com/';
+
+console.log(baseURL);
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL,
 });
 
 export default api;
